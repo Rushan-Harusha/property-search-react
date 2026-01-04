f811decf13ebfb8544c3b330306b2b1bd2f4bd80
Rushan <rushanharusha123@gmail.com>
2026-01-04 23:16:42 +0530

    fix: use HashRouter for github pages routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import SearchPage from "./pages/SearchPage";
 import PropertyPage from "./pages/PropertyPage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<SearchPage />} />
         <Route path="/property/:id" element={<PropertyPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
